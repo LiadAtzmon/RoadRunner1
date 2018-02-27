995fec1a1fb4b226b4ed064eed5fdf13b932ee34
LiadAtzmon <36335383+LiadAtzmon@users.noreply.github.com>
2018-02-27 20:17:59 +0200

    Gameover
    Gameover

---
+++ RoadRunner/Assets/GAMEOVER.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GAMEOVER : MonoBehaviour {
+
+        
+    
+	// Use this for initialization
+	void Start () {
+
+        GetComponent<TextMesh>().text = "";
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
